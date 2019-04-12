a58911a2f2a2ce252df41782afaead9ecbc1c265
Anthony Sciarini <sciarian@mail.gvsu.edu>
2019-04-12 17:06:53 -0400

    first commit

---
+++ precache-manifest.89f6df030c514cb231c2e57cb855b5ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/~sciarian/cs371/world/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d899571791a85f1196cc",
+    "url": "/~sciarian/cs371/world/static/js/runtime~main.a59f1619.js"
+  },
+  {
+    "revision": "768f44a93a093f0b62d5",
+    "url": "/~sciarian/cs371/world/static/js/main.98022c85.chunk.js"
+  },
+  {
+    "revision": "33375d65d79d401e5811",
+    "url": "/~sciarian/cs371/world/static/js/2.299d48bb.chunk.js"
+  },
+  {
+    "revision": "768f44a93a093f0b62d5",
+    "url": "/~sciarian/cs371/world/static/css/main.1b11c694.chunk.css"
+  },
+  {
+    "revision": "dd19eda1581e36a52933fb4440591c03",
+    "url": "/~sciarian/cs371/world/index.html"
+  }
+];
